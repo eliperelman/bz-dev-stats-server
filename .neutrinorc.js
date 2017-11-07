30f45252199c06f445aa419c5eaa792a1202e64e
Eli Perelman <eli@eliperelman.com>
2017-11-07 12:17:24 -0600

    Comment custom entry

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -4,6 +4,7 @@ module.exports = {
   use: [
     'neutrino-preset-node',
     (neutrino) => {
+      // custom cron job
       neutrino.config
         .entry('run-fetch-job')
           .add(join(neutrino.options.source, 'runFetchJob.js'));
